b95ffdeab3e8e7d15fa97d61340a909fdb39228b
yosnak13 <yoshihisa.nakai@ft-labo.co.jp>
2022-08-12 22:41:56 +0900

    rspec install

---
+++ backend/spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    name { "sample" }
+    email { "sample@example.com" }
+    password { "password" }
+    password_confirmation { "password" }
+  end
+end
