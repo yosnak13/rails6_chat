f0cb0b782e097170bfb81a3363129ca847b6d379
yosnak13 <yoshihisa.nakai@ft-labo.co.jp>
2022-08-13 00:55:47 +0900

    エラーテストを追加

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -11,5 +11,19 @@ RSpec.describe User, type: :model do
         expect(expected).to be_valid
       end
     end
+
+    context "ユーザー名が不正な場合" do
+      let(:invalid_user) { build(:user, name: "あ" * 31) }
+      it "エラーになること" do
+        expect(invalid_user).not_to be_valid
+      end
+    end
+
+    context "メールアドレスが不正な場合" do
+      let(:invalid_email) { build(:user, email: "example.com") }
+      it "エラーになること" do
+        expect(invalid_email).not_to be_valid
+      end
+    end
   end
 end
