c9d77e58cb28008273aaef6b5f56e7383aa9797c
yosnak13 <yoshihisa.nakai@ft-labo.co.jp>
2022-08-14 09:31:27 +0900

    テスト名変更

--- backend/spec/requests/messages_spec.rb
+++ backend/spec/requests/messages_spec.rb
@@ -5,19 +5,11 @@ require 'rails_helper'
 RSpec.describe 'Messages', type: :request do
   describe '#index' do
     context 'getリクエストが正しいとき' do
-      let(:expected) { build(:message) }
-      # let(:message_mock) { Message.new }
-      let(:params) { JSON.parse(expected.to_json) }
+      let(:params) { build(:message) }
 
-      # before do
-      #   allow(Message).to receive(:new).and_return(message_mock)
-      #   allow(message_mock).to receive(:index).with(expected).and_return(:params)
-      # end
-
-      it 'jsonを返すこと' do
-        get "http://localhost:3000/messages"
+      it 'httpステータスが200になること' do
+        get messages_path
         expect(status).to be(200)
-        # expect(actual[:content]).to be(expected)
       end
     end
   end
