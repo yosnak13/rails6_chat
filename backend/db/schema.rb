7be3e41572086274619b5146df057daee565ad8d
yosnak13 <yoshihisa.nakai@ft-labo.co.jp>
2022-08-20 18:48:58 +0900

    rubocop指摘を修正

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,8 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_20_093710) do
-
+ActiveRecord::Schema.define(version: 20_220_820_093_710) do
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "message_id"
@@ -39,7 +40,6 @@ ActiveRecord::Schema.define(version: 2022_08_20_093710) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+    t.index %w[uid provider], name: "index_users_on_uid_and_provider", unique: true
   end
-
 end
