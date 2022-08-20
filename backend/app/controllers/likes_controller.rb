1d5251ad82126371236074de4caa03be8aed7d85
yosnak13 <yoshihisa.nakai@ft-labo.co.jp>
2022-08-20 18:58:13 +0900

    likes_controller実装とroutes設定

---
+++ backend/app/controllers/likes_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class LikesController < ApplicationController
+  before_action :authenticate_user!, only: ["create"]
+
+  def create
+    like = Like.new(message_id: params[:id], user_id: current_user.id)
+
+    if like.save
+      render json: { id: like.id, email: current_user.email, message: "成功しました" }, status: 200
+    else
+      render json: { message: "保存出来ませんでした", errors: like.errors.messages }, status: 400
+    end
+  end
+end
