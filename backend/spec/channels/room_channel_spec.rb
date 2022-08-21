ca40ced337507f3a3e6da2b1aa8060c7895b56d7
yosnak13 <yosnak13@gmail.com>
2022-08-21 13:40:18 +0900

    rubocop設定でメソッドの長さ制限とAbcテストスコア加減を変更

--- backend/spec/channels/room_channel_spec.rb
+++ backend/spec/channels/room_channel_spec.rb
@@ -4,7 +4,6 @@ require "rails_helper"
 
 RSpec.describe RoomChannel, type: :channel do
   xdescribe "メッセージの送受信" do
-
     context "正常時" do
       let!(:user) { build(:user) }
       let!(:message) { build(:message, user_id: user.id) }
@@ -16,7 +15,7 @@ RSpec.describe RoomChannel, type: :channel do
       it "メッセージが保存されること" do
         expect do
           perform :post, message: message.content
-        end. to change(Message, :count).by(1)
+        end.to change(Message, :count).by(1)
       end
     end
   end
